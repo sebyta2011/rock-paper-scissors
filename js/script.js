5b59a4180d81c284f4a9647f10dee67e18d5d170
Sebastian <sebytanuniez@gmail.com>
2023-03-13 19:37:21 -0300

    Machine chooses weapon

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("Hello World!")
+
+/**machine chooses a random weapon
+ * define rules of the game
+ *  User inputs a weapon
+ 
+ compare both weapons
+ console returns who won
+ */
+
+ let getComputerChoice = Math.random() * 10 + 1;
+ 
+ for (getComputerChoice;;) {
+    if (getComputerChoice < 4){
+        getComputerChoice = 'scissors';
+    }
+    else if (getComputerChoice < 7){
+        getComputerChoice = 'paper';
+    }
+    else if (getComputerChoice < 11) {
+        getComputerChoice = 'rock';
+    }
+    else {
+        getComputerChoice == 'scissors'
+    }
+    console.log(getComputerChoice)
+    break;
+ }
+ 
