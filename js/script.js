5e5e454f1ce5d1eedecbc998f56ac9eb092fca69
Sebastian <sebytanuniez@gmail.com>
2023-03-13 20:43:28 -0300

    Working game

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 console.log("Hello World!")
 
 /**machine chooses a random weapon
+ * User inputs a weapon
  * define rules of the game
- *  User inputs a weapon
  
  compare both weapons
  console returns who won
  */
 
  let getComputerChoice = Math.random() * 10 + 1;
+ let computerChoice;
+
+ if (getComputerChoice < 4){
+    computerChoice = 'scissors';
+ } else if (getComputerChoice < 7) {
+    computerChoice = 'paper';
+ } else {
+    computerChoice = 'rock';
+ }
+console.log(computerChoice);
+
+ let playerInput = 'rock';
+ let playerChoice = playerInput.toLowerCase();
  
- for (getComputerChoice;;) {
-    if (getComputerChoice < 4){
-        getComputerChoice = 'scissors';
+ function game(playerChoice, computerChoice) {
+    if ((playerChoice === 'scissors') && (computerChoice === 'rock') ||
+    ((playerChoice === 'rock') && (computerChoice === 'paper')) ||
+    ((playerChoice === 'paper') && (computerChoice=== 'scissors'))) {
+        return "You lost";
     }
-    else if (getComputerChoice < 7){
-        getComputerChoice = 'paper';
-    }
-    else if (getComputerChoice < 11) {
-        getComputerChoice = 'rock';
+    else if 
+        ((playerChoice === 'scissors') && (computerChoice === 'paper') ||
+        (playerChoice === 'rock') && (computerChoice === 'scissors') ||
+        (playerChoice === 'paper') && (computerChoice === 'rock')) {
+            return "You won lol";
+        
     }
     else {
-        getComputerChoice == 'scissors'
+        return 'tie lol';
     }
-    console.log(getComputerChoice)
-    break;
+
  }
- 
