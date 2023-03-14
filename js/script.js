2c3673eb422a0eaa77f74dd8277458c8bd58c1b6
Sebastian <sebytanuniez@gmail.com>
2023-03-14 09:56:14 -0300

    Added player and computer score

--- js/script.js
+++ js/script.js
@@ -8,39 +8,42 @@ console.log("Hello World!")
  console returns who won
  */
 
- let getComputerChoice = Math.random() * 10 + 1;
- let computerChoice;
+let getComputerChoice = Math.random() * 10 + 1;
+let computerChoice;
 
- if (getComputerChoice < 4){
-    computerChoice = 'scissors';
- } else if (getComputerChoice < 7) {
-    computerChoice = 'paper';
- } else {
-    computerChoice = 'rock';
- }
+if (getComputerChoice < 4){
+   computerChoice = 'scissors';
+} else if (getComputerChoice < 7) {
+   computerChoice = 'paper';
+} else {
+   computerChoice = 'rock';
+}
 console.log(computerChoice);
 
- let playerInput = 'rock';
- let playerChoice = playerInput.toLowerCase();
+let playerInput = prompt('Choose your weapon');
+let playerChoice = playerInput.toLowerCase();
+
+let playerScore = 0;
+let computerScore = 0;
  
- function game(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
    if ((playerChoice === 'scissors') && (computerChoice === 'rock')) {
-      return "You lost, rock beats scissors"
+      return computerScore += 1, "You lost, rock beats scissors";
     }
    else if ((playerChoice === 'rock') && (computerChoice === 'paper')) {
-      return "You lose fool, paper beats rock!"
+      return computerScore += 1, "You lose fool, paper beats rock!"
     }
    else if ((playerChoice === 'paper') && (computerChoice=== 'scissors')) {
-      return "You lost!!!!! SCISSORS BEAT PAPER DON'T YOU REMEMBER?";
+      return computerScore += 1, "You lost!!!!! SCISSORS BEAT PAPER DON'T YOU REMEMBER?";
     }
    else if ((playerChoice === 'scissors') && (computerChoice === 'paper')) {
-      return "You won, unbelievable... scissors > paper"
+      return playerScore += 1, "You won, unbelievable... scissors > paper"
         }
    else if ((playerChoice === 'rock') && (computerChoice === 'scissors')) {
-      return "You're lucky, rock beats scissors this time"
+      return playerScore += 1, "You're lucky, rock beats scissors this time"
         }
    else if ((playerChoice === 'paper') && (computerChoice === 'rock')) {
-      return "You won lol... p>r";
+      return playerScore += 1, "You won lol... p>r";
         
     }
    else if ((playerChoice === 'scissors' && computerChoice === 'scissors')) {
@@ -56,3 +59,18 @@ console.log(computerChoice);
       return "I BEG YOU ENTER A SUITABLE WEAPON, PLEASE. PLEASE"
    }
 }
+
+function game(i) {
+   for (i = 1; i < 5; i++) {
+      return playRound(playerChoice, computerChoice)
+   }
+}
+
+
+/** player is asked for a weapon
+machine is asked for a weapon
+compare the weapons
+alert the result and add 1 to the scoreboard
+reset and make another round
+loop four times
+alert the result of the match **/
