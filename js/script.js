1484ce0b53e236a88de7e80193ff1eeaa59a2a9d
Sebastian <sebytanuniez@gmail.com>
2023-03-14 09:31:42 -0300

    Round result now has a personalized message

--- js/script.js
+++ js/script.js
@@ -24,20 +24,35 @@ console.log(computerChoice);
  let playerChoice = playerInput.toLowerCase();
  
  function game(playerChoice, computerChoice) {
-    if ((playerChoice === 'scissors') && (computerChoice === 'rock') ||
-    ((playerChoice === 'rock') && (computerChoice === 'paper')) ||
-    ((playerChoice === 'paper') && (computerChoice=== 'scissors'))) {
-        return "You lost";
+   if ((playerChoice === 'scissors') && (computerChoice === 'rock')) {
+      return "You lost, rock beats scissors"
     }
-    else if 
-        ((playerChoice === 'scissors') && (computerChoice === 'paper') ||
-        (playerChoice === 'rock') && (computerChoice === 'scissors') ||
-        (playerChoice === 'paper') && (computerChoice === 'rock')) {
-            return "You won lol";
+   else if ((playerChoice === 'rock') && (computerChoice === 'paper')) {
+      return "You lose fool, paper beats rock!"
+    }
+   else if ((playerChoice === 'paper') && (computerChoice=== 'scissors')) {
+      return "You lost!!!!! SCISSORS BEAT PAPER DON'T YOU REMEMBER?";
+    }
+   else if ((playerChoice === 'scissors') && (computerChoice === 'paper')) {
+      return "You won, unbelievable... scissors > paper"
+        }
+   else if ((playerChoice === 'rock') && (computerChoice === 'scissors')) {
+      return "You're lucky, rock beats scissors this time"
+        }
+   else if ((playerChoice === 'paper') && (computerChoice === 'rock')) {
+      return "You won lol... p>r";
         
     }
-    else {
-        return 'tie lol';
+   else if ((playerChoice === 'scissors' && computerChoice === 'scissors')) {
+      return "Scissors clash and both enjoy it, but none wins"
     }
-
- }
+   else if ((playerChoice === 'rock') && computerChoice === 'rock') {
+      return "the rocks break when they collide with the strength of a thousand horses. Both get destroyed, and both lose, that's a tie afterall"
+   }
+   else if ((playerChoice === 'paper') && computerChoice === 'paper') {
+      return "you suck, paper vs paper = tie; do better"
+   }
+   else {
+      return "I BEG YOU ENTER A SUITABLE WEAPON, PLEASE. PLEASE"
+   }
+}
